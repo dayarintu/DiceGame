5f5496b48fe8abeeefc120c068ce132989cf9973
dhaya <dhayareddy89@gmail.com>
2017-11-27 21:15:04 -0500

    added random function

--- DiceGame/DiceGame/ViewController.swift
+++ DiceGame/DiceGame/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    let diceArray = ["dice1" , "dice2" , "dice3" , "dice4" , "dice5" , "dice6"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        randomDice()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollBtnPressed(_ sender: UIButton) {
+        randomDice()
+    }
+    func randomDice() {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
 }
 
